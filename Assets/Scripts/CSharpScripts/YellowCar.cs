07488a60093e61f6d257f0d23fb54b512cc77c10
gabepena <gpena002@ucr.edu>
2015-02-05 17:45:46 -0800

    A

--- Assets/Scripts/CSharpScripts/YellowCar.cs
+++ Assets/Scripts/CSharpScripts/YellowCar.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class YellowCar : MonoBehaviour {
 
+	public GameObject waypoint;
 	int vel = 0;
 	public int maxSpeed;
 	int revSpeed = 8;
@@ -14,20 +15,14 @@ public class YellowCar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("up"))
-		{
-			vel++;
-		}
-		else if(Input.GetKey("down"))
-		{
-			vel--;
-		}
-
-		if( Input.GetKey("left") && vel != 0)
+		float temp = Mathf.Abs(waypoint.rigidbody.position.z);
+		float temp2 = Mathf.Abs (rigidbody.position.z);
+		vel = (int)(((Mathf.Abs (temp - temp2)) / (temp + temp2)) * maxSpeed);
+		if( waypoint.rigidbody.position.x <= rigidbody.position.x && vel != 0)
 		{
 			transform.Rotate(Vector3.up, -1);
 		}
-		else if( Input.GetKey("right") && vel != 0)
+		else if( waypoint.rigidbody.position.x >= rigidbody.position.x && vel != 0)
 		{
 			transform.Rotate(Vector3.up, 1);
 		}
@@ -50,7 +45,7 @@ public class YellowCar : MonoBehaviour {
 
 	void OnCollision(Collider other)
 	{
-		if( other.gameObject.layer == 8)
+		if( other.gameObject.tag == "Fence")
 		{
 			if( vel > 0) vel = -1;
 			else if( vel < 0) vel = 1;
