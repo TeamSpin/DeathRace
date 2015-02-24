dab4cccbf84a45ec59c5258fa17d21ce18978f65
Jordon Ng <jordon.s.ng@gmail.com>
2015-02-24 13:24:24 -0800

    Added Volume Manager / Music Fade In
    Click the check box to toggle music

---
+++ Assets/VolumeManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeManager : MonoBehaviour 
+{
+	public float fade;
+	bool music_status = true;
+
+	void Start() 
+	{
+		StartCoroutine(FadeInAudio (fade));
+	}
+
+	IEnumerator FadeInAudio(float fade_timer)
+	{
+		float i = 0.0f;
+		float volume_add = 1.0f / fade_timer;
+		while(i <= 1.0f) 
+		{
+			i += volume_add * Time.deltaTime;
+			audio.volume = Mathf.Lerp(0.0f, 1.0f, i);	
+			yield return new WaitForSeconds(volume_add * Time.deltaTime);
+		}
+	}
+
+	public void Audio_Toggle()
+	{
+		music_status = !music_status;
+		if(music_status == true) audio.Play();
+		else audio.Pause();
+	}
+}
