a3d65fbade0827d5f11d99da57cef8d09178c39b
gabepena <gpena002@ucr.edu>
2015-02-28 14:53:31 -0800

    Laptop Commit

--- Assets/Scripts/CSharpScripts/AI.cs
+++ Assets/Scripts/CSharpScripts/AI.cs
@@ -3,16 +3,15 @@ using System.Collections;
 
 public class AI : MonoBehaviour {
 	public Transform[] waypoints;
-	float waypointRadius = 3.0f;
+	float waypointRadius = 20.0f;
 	float damping = 0.1f;
-	bool loop = false;
-	float speed = 2.0f;
+	float speed = 1.0f;
+	float initSpeed = 0.0f;
 	bool faceHeading = true;
 	private Vector3 targetHeading;
 	private Vector3 currentHeading;
 	private int targetwaypoint;
 	private Transform xform; 
-	private bool useRigidbody; 
 	private Rigidbody rigidmember;
 
 	// Use this for initialization
@@ -24,40 +23,46 @@ public class AI : MonoBehaviour {
 			enabled = false; 
 		} 
 		targetwaypoint = 0; 
-		if(rigidbody!=null) { 
-			useRigidbody = true; 
-			rigidmember = rigidbody; 
-		} 
-		else { useRigidbody = false; }
+		rigidmember = rigidbody; 
 	}
 
 	void FixedUpdate() { 
 		targetHeading = waypoints[targetwaypoint].position - xform.position;
-		currentHeading = Vector3.Slerp(currentHeading,targetHeading,damping);
+		currentHeading = Vector3.Slerp (currentHeading, targetHeading, damping);
+//		float changeX = (Mathf.Abs (waypoints [targetwaypoint].position.x - xform.position.x)) / xform.position.x;
+//		float changeZ = (Mathf.Abs (waypoints [targetwaypoint].position.z - xform.position.z)) / xform.position.z;
+//		if (changeX / changeZ > 0.5 && changeX / changeZ < 2)
+//			speed = 2.0f;
+//		else {
+//			speed = 1.5f;
+//		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(useRigidbody)
-			rigidmember.velocity = currentHeading * speed;
-		else
-			xform.position +=currentHeading * Time.deltaTime * speed;
+		if (initSpeed < speed)
+			initSpeed += 0.1f;
+		if (initSpeed > speed)
+			initSpeed = speed;
+		xform.position +=currentHeading * Time.deltaTime * initSpeed;
 		if(faceHeading)
 			xform.LookAt(xform.position+currentHeading);
 //		Debug.Log (Vector3.Distance(xform.position,waypoints[targetwaypoint].position));
 //		Debug.Log (waypointRadius);
 //		Debug.Log (targetwaypoint);
+//		Debug.Log (rigidmember.velocity);
+//		Debug.Log (currentHeading.magnitude);
+//		Debug.Log (initSpeed);
 		if(Vector3.Distance(xform.position,waypoints[targetwaypoint].position)<=waypointRadius)
 		{
 			targetwaypoint++;
 			if(targetwaypoint>=waypoints.Length)
 			{
 				targetwaypoint = 0;
-//				if(!loop)
-//					enabled = false;
 			}
 		}
 	}
+
 	void OnDrawGizmos(){
 		
 		Gizmos.color = Color.red;
