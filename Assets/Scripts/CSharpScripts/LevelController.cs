6057804763377ae74a8217c2374988f89dcca0f4
Jordon Ng <jordon.s.ng@gmail.com>
2015-03-07 15:31:16 -0800

    Added Basic SplitScreen + Mini Map
    Still needs separate controllers

--- Assets/Scripts/CSharpScripts/LevelController.cs
+++ Assets/Scripts/CSharpScripts/LevelController.cs
@@ -5,6 +5,11 @@ public class LevelController : MonoBehaviour
 {
 	public void SceneSwitch (int scene) 
 	{
+		if(scene == 0)
+		{
+			GameObject old_audio = GameObject.Find("VolumeManager");
+			Destroy(old_audio);
+		}
 		Application.LoadLevel (scene);
 	}
 }
